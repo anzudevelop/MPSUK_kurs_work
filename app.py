70c697e996d1e2eb335f0b6e1c752b9fe18dcde9
anzudevelop <72302256+anzudevelop@users.noreply.github.com>
2023-12-12 16:33:30 +0700

    send image HTTP

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
+#pip install Flask
+#pip install --upgrade Pillow
+import os
 from PIL import Image
+from flask import Flask, render_template, request, send_from_directory
+
+app = Flask(__name__)
+
+
 def calculate_brightness(image):
     greyscale_image = image.convert('L')
     histogram = greyscale_image.histogram()
@@ -11,13 +19,32 @@ def calculate_brightness(image):
         brightness += ratio * (-scale + index)
     return 1 if brightness == 255 else brightness / scale
 
-imag1 = Image.open("test1.jpg")
-imag2 = Image.open("test2.jpg")
-imag3 = Image.open("test3.jpg")
-imag_white = Image.open("white.jpg")
-imag_black = Image.open("black.jpg")
-print(calculate_brightness(imag1))
-print(calculate_brightness(imag2))
-print(calculate_brightness(imag3))
-print(calculate_brightness(imag_white))
-print(calculate_brightness(imag_black))
+image1 = Image.open("test1.jpg")
+image2 = Image.open("test2.jpg")
+image3 = Image.open("test3.jpg")
+image_white = Image.open("white.jpg")
+image_black = Image.open("black.jpg")
+print(calculate_brightness(image1))
+print(calculate_brightness(image2))
+print(calculate_brightness(image3))
+print(calculate_brightness(image_white))
+print(calculate_brightness(image_black))
+
+UPLOAD_FOLDER = 'img'
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route("/get_image", methods=['GET', 'POST'])
+def get_image():
+    if request.method == 'GET':
+        print("Отправка фото...")
+        return send_from_directory(UPLOAD_FOLDER, 'image.jpg')
+
+
+
+
+
+app.run(host='0.0.0.0', port=5555)
