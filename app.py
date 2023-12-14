a2a4e74e356d4b392f21b8e5524de00875414200
anzudevelop <72302256+anzudevelop@users.noreply.github.com>
2023-12-14 22:03:24 +0700

    work with raspberry

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-#pip install Flask
-#pip install --upgrade Pillow
-#pip install flask-ngrok
 import os
+import subprocess
 from PIL import Image
 from flask import Flask, render_template, request, send_from_directory
 from flask_ngrok import run_with_ngrok 
 import random
 
+#home_dir = os.system("fswebcam --no-banner -r 480x272 test1.jpg")
+#list_files = subprocess.run(["ls"])
+#os.system("fswebcam --no-banner -r 480x272 test1.jpg")
+
 app = Flask(__name__)
-run_with_ngrok(app)
+#run_with_ngrok(app)
+
 
 pwm = random.random()
+noPhoto = True
 
 def calculate_brightness(image):
     greyscale_image = image.convert('L')
@@ -24,19 +28,22 @@ def calculate_brightness(image):
         brightness += ratio * (-scale + index)
     return 1 if brightness == 255 else brightness / scale
 
-image1 = Image.open("test1.jpg")
-image2 = Image.open("test2.jpg")
-image3 = Image.open("test3.jpg")
-image_white = Image.open("white.jpg")
-image_black = Image.open("black.jpg")
-print(calculate_brightness(image1))
-print(calculate_brightness(image2))
-print(calculate_brightness(image3))
-print(calculate_brightness(image_white))
-print(calculate_brightness(image_black))
+def deletePhoto():
+    os.system("rm filename image.jpg")
+    global noPhoto
+    noPhoto = True
+# image1 = Image.open("test1.jpg")
+# image2 = Image.open("test2.jpg")
+# image3 = Image.open("test3.jpg")
+# image_white = Image.open("white.jpg")
+# image_black = Image.open("black.jpg")
+# print(calculate_brightness(image1))
+# print(calculate_brightness(image2))
+# print(calculate_brightness(image3))
+# print(calculate_brightness(image_white))
+# print(calculate_brightness(image_black))
 
-UPLOAD_FOLDER = 'img'
-
+UPLOAD_FOLDER = ''
 
 @app.route("/")
 def index():
@@ -44,9 +51,12 @@ def index():
 
 @app.route("/get_image", methods=['GET', 'POST'])
 def get_image():
-    if request.method == 'GET':
-        print("Отправка фото...")
-        return send_from_directory(UPLOAD_FOLDER, 'image.jpg')
+    print("Делаю снимок")
+    os.system("fswebcam --no-banner -r 480x272 image.jpg")
+    global noPhoto
+    noPhoto = False
+    print("Отправка фото...")
+    return send_from_directory(UPLOAD_FOLDER, 'image.jpg')    
 
 @app.route("/logs", methods=['GET'])
 def logs():
@@ -57,52 +67,13 @@ def logs():
         status = 200,
         mimetype='text/html'
     )
+    if(noPhoto == False):
+        deletePhoto()
     return response
 
 
 
 if __name__ == "__main__":
-    app.run()
+    #app.run()
+    app.run(host='0.0.0.0', port=5555)
 
-
-
-
-
-
-
-
-
-
-'''
-@app.route("/getGpioState", methods=["get"])
-def getGpioState():
-    pin = request.args.get('pin')
-    global gpioState
-    if gpioState:
-        state = "on"
-    else:
-        state = "off"
-    print("gpio pin " + pin + " state = " + state)
-     
-    response = app.response_class(
-        response = state,
-        status = 200,
-        mimetype='text/html'
-    )
-    return response
-
-@app.route("/setStringToFile", methods=["post"])
-def setStringToFile():
-    data = request.get_json()
-    text = data.get('text')
-    print("new text to file is: " + text)
-    file = open('text.txt', 'w')
-    file.write(text)
-    file.close()
-    response = app.response_class(
-        response = 'ok',
-        status = 200,
-        mimetype='text/html'
-    )
-    return response
-'''
